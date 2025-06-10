8eb625a93dbfdc11f8e804565151b167f480823e
petelakph <116359328+petelakph@users.noreply.github.com>
2025-06-10 09:23:52 -0300

    adicionando files do projeto anterior

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Games',
+    'Em que ano foi lançado Resident Evil 3?',
+    'Resident Evil 3 foi lançado em 1999'
+)
+
+criaCartao(
+    'Games',
+    'Qual o nome completo do CJ no GTA San Andreas?',
+    'O nome completo do CJ é Carl Johnson'
+)
+
+criaCartao(
+    'Games',
+    'Quais são as 3 rotas principais de Undertale?',
+    'As 3 rotas são Neutra, Pacifista e Genocida'
+)
+
+criaCartao(
+    'Games',
+    'Qual o nome das duas personagens principais de The Last Of Us 2?',
+    'O nome das duas personagens principais são Ellie e Abby'
+)
+
+criaCartao(
+    'Games',
+    'Qual a doença terminal que o Arthur Morgan obteve?',
+    'Tuberculose.'
+)
